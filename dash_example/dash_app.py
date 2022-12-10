0cab01e5cac88d8c6bd1a06a59709610136aba12
Emir Burgos Paz <47346304+EmirBurgosPaz@users.noreply.github.com>
2022-12-09 21:17:19 -0600

    diseño del dash

--- dash_example/dash_app.py
+++ dash_example/dash_app.py
@@ -10,27 +10,23 @@ import plotly.graph_objects as go
 app = dash.Dash(__name__)
 
 app.layout = html.Div([
-        html.H1("Escribe un valor para modificar la grafica"),
-        html.H3([
-        "Valor: ",
-        dcc.Input(id='lambda', value=1, type='text')
-        ]),
+        html.H1("Modelador simple de las ventas historicas del año movil"),
+        html.Tr([html.Td(["Incremento de pzs vendidas:"]),html.Td([ dcc.Input(id='lambda', value=1, type='text')])]),
+        html.Tr([html.Td(["Incremento del precio: "]),html.Td([ dcc.Input(id='lambda', value=1, type='text')])]),
     html.Hr(),
-    html.Br(),
-    html.Div(dcc.Graph(id='histogram')),
-    html.Hr(),
+    html.Tr([html.Td(html.H3(id='value'),className="expanded"), html.Td(html.H3(id='units'))]),
+    html.Div([dcc.Graph(id='histogram')],className= "container forward")
     ],className="container animated")
-
-
-
 
 
 @app.callback(
    Output("histogram", "figure"),
+    Output("value", "children"),
+    Output("units", "children"),
     Input("lambda", "value"),
 )
 def graph_histogram(lambda_value):
-    if lambda_value:
+    if lambda_value != "":
         s = pd.date_range(end = datetime.today(), periods = 20).to_pydatetime().tolist()
 
         sales = np.random.default_rng().uniform(1000,1200,20)
@@ -39,22 +35,29 @@ def graph_histogram(lambda_value):
         for index,item in enumerate(sales):
             sales[index] = item * float(lambda_value)
             #print(lambda_value)
-        fig =make_subplots(rows=2, cols=1)
+        fig =make_subplots(rows=1, cols=1)
         fig.append_trace(go.Scatter(x=s,y=sales,marker= dict(
                 color="#9e2f7f"
                 )),1,1)
         fig.append_trace(go.Scatter(x=s,y=sales_2,marker= dict(
                 color="#e75263"
                 )),1,1)
-        fig.update_layout(paper_bgcolor="#1d2228",plot_bgcolor="#1d2228",
+        fig.update_layout(title="Ventas por mes",
+                xaxis_title="Fecha",
+                yaxis_title="Venta",
+                autosize=False,
+                width=1400,
+                height=500,
+                paper_bgcolor="#1d2228",
+                plot_bgcolor="#1d2228",
                         font=dict(
                             family="Courier New, monospace",
-                            size=18,
+                                size=18,
                             color="#fd9668"
         ))
         fig.update_xaxes(showline=False, linewidth=2, gridcolor='#49bf9d')
         fig.update_yaxes(showline=False, linewidth=2, gridcolor='#49bf9d')
-        return fig
+        return fig, f'Margen anual: {2}',f'Pzs extras a vender: {2}'
     else:
         return dash.no_update
 
