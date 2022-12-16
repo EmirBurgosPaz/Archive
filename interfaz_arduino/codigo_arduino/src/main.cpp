cdb59ed6301632093cc974efb24f1fd92d825e8c
Emir Burgos Paz <47346304+EmirBurgosPaz@users.noreply.github.com>
2022-12-16 03:12:26 -0600

    se agrego un controlador para los rgb, y memoria volatil simple

--- interfaz_arduino/codigo_arduino/src/main.cpp
+++ interfaz_arduino/codigo_arduino/src/main.cpp
@@ -4,7 +4,15 @@
 SoftwareSerial SerialNextion(2, 3); // RX, TX
 String  endChar = String(char(0xff)) + String(char(0xff)) + String(char(0xff));
 
-long valor = 128;
+long red_value = 128;
+long green_value = 128;
+long blue_value = 128;
+int motor_state = 0;
+
+int ledRojo = 9;
+int ledVerde = 10;
+int ledAzul = 11;
+
 String dfd;
 
 union main
@@ -13,43 +21,93 @@ union main
   long valLong;
 }value;
 
+void nextionCommand(String nextionName, long value);
 
 void setup() {
   SerialNextion.begin(9600);                       //The default baud rate of the Nextion TFT is 9600.         
   Serial.begin(9600);
-  pinMode(LED_BUILTIN, OUTPUT);  
+  pinMode(LED_BUILTIN, OUTPUT); 
+  pinMode(ledRojo,OUTPUT);
+  pinMode(ledVerde,OUTPUT);
+  pinMode(ledAzul,OUTPUT);
+  pinMode(13, OUTPUT); 
+
+  analogWrite(ledRojo,red_value); 
+  analogWrite(ledVerde,green_value); 
+  analogWrite(ledAzul,ledAzul); 
+
 }
 
 void loop() {
-  if(SerialNextion.available()>0)                  //If we receive something...
+digitalWrite(13, LOW);
+
+if(SerialNextion.available()>0)                  //If we receive something...
   {
     dfd += char(SerialNextion.read());
     Serial.print(dfd);
     Serial.println(dfd.length());
+    Serial.print(String(value.valLong));
   }
 
-  if ((dfd.substring(0,3)=="set") & (dfd.length()==3))
-  {
-    SerialNextion.print("h2.val=");
-    SerialNextion.print(String(valor));
-    SerialNextion.print(endChar);
+ 
+
+if((dfd.substring(0,5)=="green")& (dfd.length()==9)){
+
+    value.charByte[0]= char(dfd[5]);
+
+    green_value = value.valLong;  
+    Serial.println(String(green_value));
+    analogWrite(ledVerde,green_value); 
+
     dfd="";
   }
-  
 
-  if((dfd.substring(0,3)=="red")& (dfd.length()==7)){
+if((dfd.substring(0,4)=="blue")& (dfd.length()==8)){
+
+    value.charByte[0]= char(dfd[4]);
+
+    blue_value = value.valLong;  
+    Serial.println(String(blue_value));
+    analogWrite(ledAzul,blue_value); 
+
+    dfd="";
+  }
+
+if((dfd.substring(0,3)=="red")& (dfd.length()==7)){
 
     value.charByte[0]= char(dfd[3]);
 
-    valor = value.valLong;
+    red_value = value.valLong; 
+    analogWrite(ledRojo,red_value); 
+    dfd="";
+  }
 
-    Serial.println(String(value.valLong));
-
-    SerialNextion.print("h1.val=");
-    SerialNextion.print(String(value.valLong));
-    SerialNextion.print(endChar);
-    
+if ((dfd.substring(0,3)=="set") & (dfd.length()==3))
+  {
+    nextionCommand("h0.val", red_value);
+    delay(100);
+    nextionCommand("h1.val", green_value);
+    delay(100);
+    nextionCommand("h2.val", blue_value);
     dfd="";
   }
   
+
+if((dfd.substring(0,5)=="motor")&(dfd.length()==9)){
+
+    value.charByte[0]= char(dfd[5]);
+
+    long(motor_state) = value.valLong;  
+    if(motor_state==1) digitalWrite(LED_BUILTIN, HIGH);
+    if(motor_state==0) digitalWrite(LED_BUILTIN, LOW);
+    dfd="";
+  }
+  
+}
+
+void nextionCommand(String nextionName, long value){
+  SerialNextion.print(nextionName);
+  SerialNextion.print("=");
+  SerialNextion.print(String(value));
+  SerialNextion.print(endChar);
 }
