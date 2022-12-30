aba6a5af91d6bd93d591dafa81e401dd3389177d
Emir Burgos Paz <47346304+EmirBurgosPaz@users.noreply.github.com>
2022-12-30 00:24:43 -0600

    Se agrego un diagrama simple del sistema

--- diagram_test/diagram.py
+++ diagram_test/diagram.py
@@ -10,19 +10,77 @@ graph_attr = {
     "splines": "spline",
 }
 
-with Diagram("Interfaz Arduino-Nextion-simple", direction="TB", graph_attr=graph_attr):
+with Diagram("Interfaz Arduino-Nextion", direction="TB", graph_attr=graph_attr):
     customer = Person(name="Usuario", description="Usuario del sistema")
-    #with SystemBoundary("Interfaz"):
-    #    index = Container(
-    #        name="Indice principal",
-    #        technology="Nextion",
-    #        description="Indice para escoger variables"
-    #    )
-    Hardware = System(name="Controlador", description="Controlador de motor y focos", external=False)
-    Interfaz = System(name="Interfaz de usuario", description="Interfaz para la interaccion del usuario", external=False)
+    #
+    Controlador = System(name="Arduino", description="Recive y manda datos de la interfaz", external=False)
+    Sistema = System(name="Sistema fisico", description="Sistema que sera controlado", external=True)
+    with SystemBoundary("Interfaz"):
+        index = Container(
+            name="Indice principal",
+            technology="Nextion",
+            description="Indice para escoger variables"
+       )
 
-    customer >> Relationship("Modifica las opciones") >> Interfaz
-    Interfaz >> Relationship("Modifica las actividades") >> Hardware
+        automatico = Container(
+            name="Pagina automatico",
+            technology="Nextion",
+            description="Solo puedes modificar LEDs"
+       )
+
+        manual = Container(
+            name="Pagina manual",
+            technology="Nextion",
+            description="Se puede modificar LEDs y las horas de la alarma"
+       )
+
+        configuracion = Container(
+            name="Pagina configuracion",
+            technology="Nextion",
+            description="Te permite modifcar todo"
+       )
+
+        ModificadorLEDs = Container(
+            name="Modificador LEDs",
+            technology="Nextion",
+            description="Te permite escoger los valores RGB de los LEDs"
+       )
+        AlarmSelecter = Container(
+            name="Selector alarmas",
+            technology="Nextion",
+            description="Te permite escoger que alarma quieres modificar"
+       )
+        ModificadorHora = Container(
+            name="Modificador hora",
+            technology="Nextion",
+            description="Te permite modificar la hora"
+       )
+        MotorSelecter = Container(
+            name="Selector motor",
+            technology="Nextion",
+            description="Te permite escoger que motores quieres modificar"
+       )
+        MotoresSimples = Container(
+            name="Motor 1-5",
+            technology="Nextion",
+            description="Te permite apagar/prender los motores simples"
+       )
+        Motor6 = Container(
+            name="Modificador motor 6",
+            technology="Nextion",
+            description="Te permite modificar el motor 6"
+       )
+
+
+    customer >> Relationship("Modifica las opciones") >> index
+    index >> Relationship("Escoges la opcion deseada") >> [automatico, manual, configuracion]
+    automatico >> Relationship("Modificas solo los LEDs") >> ModificadorLEDs
+    manual >> Relationship("Modifica horas y LEDs") >> [ModificadorLEDs, AlarmSelecter]
+    AlarmSelecter >> Relationship("Escoges la alarma a modificar") >> ModificadorHora
+    configuracion >> Relationship("Modifica todo") >> [ModificadorLEDs, AlarmSelecter, MotorSelecter]
+    MotorSelecter >> Relationship("Escoges los motores a modificar") >> [MotoresSimples, Motor6]
+    [Motor6,MotoresSimples,ModificadorHora,ModificadorLEDs] >> Relationship("Recive informacion y devuelve informacion") >> Controlador
+    Controlador >> Relationship("Modifica los elementos fisicos") >> Sistema
 
 #with Diagram("Container diagram for Internet Banking System", direction="TB", graph_attr=graph_attr):
 #    customer = Person(
